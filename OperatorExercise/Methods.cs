37a38b6c39aa808ab554d08da7b803f960d82ecf
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-12 21:45:38 -0500

    initial commit

---
+++ OperatorExercise/Methods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Methods
+    {
+        public static void AreaOfCircle()
+        {
+            Console.WriteLine("Please enter the radius of your circle.");
+            var radius = double.Parse(Console.ReadLine());
+
+            var areaOfCircle = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {areaOfCircle}");
+        }
+
+    }
+}
---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,61 @@
+﻿using OperatorExercise;
+//Exercise 1
+Console.WriteLine($"-----------Exercise 1------------------");
+
+int a = 17;
+int b = 4;
+
+//Addition
+//int c = a + b;
+//Console.WriteLine(c);
+
+//Subtraction
+//int d = a - b;
+//Console.WriteLine(d);
+
+////Multiplication
+//int e = a * b;
+//Console.WriteLine(e);
+
+//Division
+int quotient = a / b;
+Console.WriteLine(quotient);
+
+//Modulus
+int remainder = a % b;
+Console.WriteLine(remainder);
+
+
+if (a == 17 && b == 4)
+{
+    Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+    
+}
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"-----------Exercise 2------------------");
+Console.WriteLine();
+Console.WriteLine();
+
+Methods.AreaOfCircle();
+
+
+
+
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine($"-----------Thought Exercise-------------------");
+Console.WriteLine();
+Console.WriteLine();
+
+var i = 3;
+var j = 4;
+var k = ++i * j++;
+
+Console.WriteLine(k);
+
+//j++ doesn't change in value unless the equation is run more than one time.  Since this isn't a loop it is only 
+//running one time.  So i increase by one to make 4 and 4*4 = 16.  Which is the answer when you run it.  If it were to run
+//twice the answer would be 25.
